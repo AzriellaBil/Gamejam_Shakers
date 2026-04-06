141937bb49409e754a8cce2cdf72b9071ba4b96a
AzriellaBil <nabilazriella@gmail.com>
2026-04-07 04:38:49 +0700

    UI Animation
    +Pause Menu And Functionality
    +Button Animation

--- Assets/Levels/Scripts/UI/UIMenu.cs
+++ Assets/Levels/Scripts/UI/UIMenu.cs
@@ -1,16 +1,46 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float TweeenSpeed;
+    [SerializeField] CanvasGroup cg;
+    private bool IsPaused = false;
+    public void Continue()
     {
-        
+      LeanTween.alphaCanvas(cg, 0f, TweeenSpeed)
+        .setEase(LeanTweenType.easeInOutSine)
+        .setOnComplete(() =>
+        {
+            IsPaused = false;
+        }
+        );
+    }
+    public void Pause()
+    {
+        IsPaused = true;
+        LeanTween.cancel(gameObject); 
+
+        LeanTween.alphaCanvas(cg, 1f, TweeenSpeed)
+        .setEase(LeanTweenType.easeInOutSine);
+    
+    }
+        void Update()
+    {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                if(IsPaused == false)
+                {
+                    Pause();
+                }
+                else
+                {
+                    Continue();
+                }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
