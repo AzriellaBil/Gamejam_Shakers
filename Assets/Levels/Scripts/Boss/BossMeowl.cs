a390e6946302e09ba20d716c7c86ce3206c7954b
AzriellaBil <nabilazriella@gmail.com>
2026-04-11 17:33:04 +0700

    BossFix

--- Assets/Levels/Scripts/Boss/BossMeowl.cs
+++ Assets/Levels/Scripts/Boss/BossMeowl.cs
@@ -6,6 +6,10 @@ public class BossMeowl : MonoBehaviour
     [Header("Stats Bos")]
     public int maxHP = 5; // Butuh bunuh 5 kroco buat ngalahin bos
     private int currentHP;
+    private int curMinion;
+    private bool BossEnabled = false;
+    [SerializeField] int maxMinion;
+    public LayerMask Ground;
 
     [Header("Sistem Summon")]
     public GameObject krocoPrefab; // Prefab musuh kroconya
@@ -18,53 +22,89 @@ public class BossMeowl : MonoBehaviour
     private Vector3 posisiAwal;
 
     private Animator anim;
+    public StringEventChannel onNewLocation;
 
-    void Start()
+    void OnEnable()
     {
-        currentHP = maxHP;
-        posisiAwal = transform.position;
-        anim = GetComponent<Animator>();
-
-        // Mulai rutinitas summon kroco
-        StartCoroutine(SummonKroco());
+        if (onNewLocation == null)
+        {
+            Debug.LogError("onNewLocation is not assigned in the Inspector!", this);
+            return;
+        }
+        onNewLocation.OnEventRaised += EnableBoss;
     }
+
+    void OnDisable()
+    {
+        onNewLocation.OnEventRaised += EnableBoss;
+    }
+
+    void EnableBoss(string Locations)
+    {
+
+        if (Locations == "Boss Stage")
+        {
+            BossEnabled = true;
+            currentHP = maxHP;
+            posisiAwal = transform.position;
+            anim = GetComponent<Animator>();
+
+            // Mulai rutinitas summon kroco
+            StartCoroutine(SummonKroco());
+
+        }
+
+    }
+
 
     void Update()
     {
-        // Bikin bosnya melayang naik-turun secara natural
-        float hoverY = Mathf.Sin(Time.time * kecepatanMelayang) * tinggiMelayang;
-        transform.position = posisiAwal + new Vector3(0, hoverY, 0);
+        if (BossEnabled == true)
+        {
+            float hoverY = Mathf.Sin(Time.time * kecepatanMelayang) * tinggiMelayang;
+            transform.position = posisiAwal + new Vector3(0, hoverY, 0);
+        }
     }
 
     private IEnumerator SummonKroco()
     {
-        while (currentHP > 0)
+        while (currentHP > 0 && curMinion <= maxMinion)
         {
             yield return new WaitForSeconds(waktuSummon);
 
             // Munculin kroconya
-            GameObject krocoBaru = Instantiate(krocoPrefab, transform.position, Quaternion.identity);
 
-            // Kasih tau kroconya siapa bosnya
-            MinionBoss scriptKroco = krocoBaru.GetComponent<MinionBoss>();
-            if (scriptKroco != null)
+            
+            RaycastHit2D hit = Physics2D.Raycast(transform.position,Vector2.down, 30f, Ground);
+            if (hit.collider != null)
             {
-                scriptKroco.bosAtasan = this; 
+                // Vector2 HitPos = hit.point + new Vector2(0f,-5f);
+                GameObject krocoBaru = Instantiate(krocoPrefab, transform.position + new Vector3(Random.Range(-10f,2f),0.2f), Quaternion.identity);
+                curMinion += 1;
+
+                // Kasih tau kroconya siapa bosnya
+                MinionBoss scriptKroco = krocoBaru.GetComponent<MinionBoss>();
+                if (scriptKroco != null)
+                {
+                    scriptKroco.bosAtasan = this;
+                }
+
+                // 3. Efek Terlempar (Disembur dari bos)
+                Rigidbody2D rbKroco = krocoBaru.GetComponent<Rigidbody2D>();
+                if (rbKroco != null)
+                {
+                    // Pilih arah X acak (ke kiri atau ke kanan)
+                    float arahX = Random.Range(-20f, 2f);
+
+                    // Bikin vektor arah lemparan (X acak, Y sedikit ke atas biar melengkung)
+                    Vector2 arahLempar = new Vector2(arahX, 1f).normalized;
+
+                    // Gunakan linearVelocity persis kayak di script Player kamu
+                    rbKroco.linearVelocity = arahLempar * kekuatanSembur;
+                }
             }
 
-            // 3. Efek Terlempar (Disembur dari bos)
-            Rigidbody2D rbKroco = krocoBaru.GetComponent<Rigidbody2D>();
-            if (rbKroco != null)
-            {
-                // Pilih arah X acak (ke kiri atau ke kanan)
-                float arahX = Random.Range(-1f, 1f); 
-                
-                // Bikin vektor arah lemparan (X acak, Y sedikit ke atas biar melengkung)
-                Vector2 arahLempar = new Vector2(arahX, 1f).normalized; 
-                
-                // Gunakan linearVelocity persis kayak di script Player kamu
-                rbKroco.linearVelocity = arahLempar * kekuatanSembur;
-            }
+
         }
     }
 
@@ -88,7 +128,7 @@ public class BossMeowl : MonoBehaviour
         StopAllCoroutines(); // Stop summon kroco
 
         if (anim != null) anim.SetTrigger("Die");
-        
-        Destroy(gameObject, 3f); 
+
+        Destroy(gameObject, 3f);
     }
 }
